d73115e61a6559512d673ce58ad73f8f1ef09bcf
Marcelo Vinicius <marcelo_vinicius7@hotmail.com>
2019-09-17 16:53:07 -0300

    Atualizado Angular 6.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,11 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {LocationStrategy, HashLocationStrategy, registerLocaleData} from '@angular/common';
+import locatePt from '@angular/common/locales/pt'
+
+registerLocaleData(locatePt, 'pt')
+
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component'
 import { ApplicationErrorHandler } from './app.error-handle';
@@ -50,7 +54,7 @@ import { ApplicationErrorHandler } from './app.error-handle';
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},{provide: LOCALE_ID, useValue: 'pt-BR'},
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},{provide: LOCALE_ID, useValue: 'pt'},
               {provide: ErrorHandler, useClass: ApplicationErrorHandler}],
   bootstrap: [AppComponent]
 })
