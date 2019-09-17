39541ecba52107a8325f5b9bd4b1dc70da4f0759
Marcelo Vinicius <marcelo_vinicius7@hotmail.com>
2019-09-17 17:24:40 -0300

    Atualizado Angular 6.0

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
+import {FormGroup, FormBuilder, FormControl, Validators, AbstractControl} from '@angular/forms'
 
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
@@ -34,15 +34,15 @@ export class OrderComponent implements OnInit {
      private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)] ),
+    this.orderForm = new FormGroup({
+      name: new FormControl('', { validators: [Validators.required, Validators.minLength(5)]}),
       email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
       emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
       address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern) ]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
-    }, {validator: OrderComponent.equalsTo})
+    }, {validators: [OrderComponent.equalsTo], updateOn: 'blur'})
   }
 
   static equalsTo(group: AbstractControl): {[key:string] : boolean} {
